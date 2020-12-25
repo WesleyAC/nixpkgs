cdfed51d0c95ac764aed9228c76785fe2362b4a6
Morgan Jones <me@numin.it>
2020-12-25 15:05:53 -0700

    ovftool: init at 4.4.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6387,6 +6387,8 @@ in
 
   otpw = callPackage ../os-specific/linux/otpw { };
 
+  ovftool = callPackage ../tools/virtualization/ovftool { };
+
   overcommit = callPackage ../development/tools/overcommit { };
 
   overmind = callPackage ../applications/misc/overmind { };
